0d650f8c27da8700583742773bc335d3cb83db0f
kakon dey <kakondey701@gmail.com>
2021-03-15 15:03:22 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,79 +1,135 @@
-import React, { useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = (props) => {
-  // console.log(slides);
-  const [current, setCurrent] = useState(0);
-
-  const length = props.slides.length;
-
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-  console.log(current);
-  if (!Array.isArray(props.slides) || props.slides.length <= 0) {
-    return null;
+  // console.log(props);
+  const [nextbutton, setNextButton] = useState(false); //previous button
+  const [slide, setSlide] = useState(props.slides[0].title);
+  const [nextslide, setNextSlide] = useState(props.slides[0].text);
+  const [activebutton, setActiveButton] = useState(true); //next button
+  const [againstart, setAgainStart] = useState(false); //restart button
+  /* ------------------------------------------*/
+  //next button functionalities start here
+  function slideFunction(slide) {
+    //setNextButton(true)
+    // console.log(slide);
+    // i++;
+    // if (i == 4) {
+    //   setActiveButton(false);
+    //   setNextButton(true);
+    // }
+    // if (i < 5) {
+    //   setSlide(props.slides[i].title);
+    //   setNextSlide(props.slides[i].text);
+    //   setNextButton(true);
+    // }
+    var found1 = props.slides;
+    var index1 = found1.findIndex((fruit1) => fruit1.title === slide);
+    // console.log(index1)
+    if (index1 == 3) {
+      setActiveButton(false);
+    }
+    if (index1 < 4) {
+      setSlide(found1[index1 + 1].title);
+      setNextSlide(found1[index1 + 1].text);
+      setNextButton(true);
+      setAgainStart(true);
+    }
   }
 
-  const reset = () => {
-    setCurrent(0);
-  };
+  //next button functionalities end here
+  /* ------------------------------------------*/
 
-  // useEffect(() => {
-  //   if(current===length-1){
+  //previous  button functionalities start here
+  function prevSlide(slide) {
+    //console.log(slide);
+    var found = props.slides;
+    // console.log(found)
 
-  //   }
-  //   return () => {
-  //     cleanup
-  //   }
-  // }, [input])
+    var index = found.findIndex((fruit) => fruit.title === slide);
+    // console.log(index);
+    if (index == 1) {
+      setNextButton(false);
+      setActiveButton(true);
+      setAgainStart(false);
+    }
+    if (index > 0) {
+      setSlide(found[index - 1].title);
+      setNextSlide(found[index - 1].text);
+      setActiveButton(true);
+    }
+  }
+  //previous  button functionalities end here
+  /* ------------------------------------------*/
+
+  //restart button functionalities starts here
+
+  //restart functionalities
+  function restart(slide) {
+    var found3 = props.slides;
+    var index3 = found3.findIndex((fruit3) => fruit3.title === slide);
+
+    // setSlide(found3[0].title)
+    //  setNextSlide(found3[0].text);
+
+    //    setAgainStart(false)
+
+    //  setNextButton(false)
+
+    if (index3 < found3.length) {
+      setSlide(found3[0].title);
+      setNextSlide(found3[0].text);
+      setAgainStart(false);
+      setActiveButton(true);
+      setNextButton(false);
+    }
+  }
+  //restart button functionalities ends here
+
   return (
     <>
-      <div className="slider">
-        {props.slides.map((image, index) => {
-          return (
-            <div
-              key={index}
-              className={index === current ? "slide active" : "slide"}
-            >
-              {index === current && (
-                <div className="content">
-                  <h1 data-testid="title">{image.title}</h1>
-                  <p data-testid="text">{image.text}</p>
-                </div>
-              )}
-            </div>
-          );
-        })}
-      </div>
+      <h1 data-testid="title">{slide}</h1>
+      <p data-testid="text"> {nextslide}</p>
+      {/* {console.log(slide)} */}
+      {/* -------------------------------------- */}
+
+      {/* previous button below */}
       <button
-        disabled={current === 0}
-        className="restart"
-        data-testid="button-restart"
-        onClick={reset}
-      >
-        Restart
-      </button>
-      <button
-        disabled={current === 0}
-        className="prev"
         data-testid="button-prev"
-        onClick={prevSlide}
+        disabled={!nextbutton}
+        onClick={() => prevSlide(slide)}
+        type="button"
       >
         Prev
       </button>
+      {/* previous button end */}
+
+      {/* -------------------------------------- */}
+
+      {/* next button below */}
       <button
-        disabled={current === length - 1}
-        className="next"
         data-testid="button-next"
-        onClick={nextSlide}
+        disabled={!activebutton}
+        type="button"
+        onClick={() => slideFunction(slide)}
       >
         Next
       </button>
+
+      {/* next button end*/}
+
+      {/* -------------------------------------- */}
+
+      {/* restart button below */}
+      <button
+        data-testid="button-restart"
+        disabled={!againstart}
+        type="button"
+        onClick={() => restart(slide)}
+      >
+        Restart
+      </button>
+      {/* restart button end */}
     </>
   );
 };
