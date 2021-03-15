0b2a0ff64dee07c5c43791f539260360280439af
kakon dey <kakondey701@gmail.com>
2021-03-15 10:40:52 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,76 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
+  // console.log(slides);
+  const [current, setCurrent] = useState(0);
+
+  const length = props.slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  console.log(current);
+  if (!Array.isArray(props.slides) || props.slides.length <= 0) {
+    return null;
+  }
+
+  const reset = () => {
+    setCurrent(0);
+  };
+
+  // useEffect(() => {
+  //   if(current===length-1){
+
+  //   }
+  //   return () => {
+  //     cleanup
+  //   }
+  // }, [input])
   return (
-    <></>
-  )
-}
-
+    <>
+      <div className="slider">
+        {props.slides.map((image, index) => {
+          return (
+            <div
+              key={index}
+              className={index === current ? "slide active" : "slide"}
+            >
+              {index === current && (
+                <div className="content">
+                  <h1 data-testid="title">{image.title}</h1>
+                  <p data-testid="text">{image.text}</p>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <button className="restart" data-testid="button-restart" onClick={reset}>
+        Restart
+      </button>
+      <button
+        disabled={current === 0}
+        className="prev"
+        data-testid="button-prev"
+        onClick={prevSlide}
+      >
+        Prev
+      </button>
+      <button
+        disabled={current === length - 1}
+        className="next"
+        data-testid="button-next"
+        onClick={nextSlide}
+      >
+        Next
+      </button>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/SliderData.js
@@ -0,0 +1,23 @@
+import image1 from "../Images/image1";
+import image2 from "../Images/image2";
+import image3 from "../Images/image3";
+import image4 from "../Images/image4";
+import image5 from "../Images/image5";
+
+export const SliderData = [
+  {
+    image: image1,
+  },
+  {
+    image: image2,
+  },
+  {
+    image: image3,
+  },
+  {
+    image: image4,
+  },
+  {
+    image: image5,
+  },
+];
